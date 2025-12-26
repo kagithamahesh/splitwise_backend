fedb2545d77c0cbe96bf9a6520fd0bda2afdbb1d
mahesh <kagithacm237@gmail.com>
2025-12-27 01:29:47 +0530

    modified the images issues

--- controllers/groupinfo.go
+++ controllers/groupinfo.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -15,19 +17,36 @@ type GroupInput struct {
 }
 
 func CreateGroup(c *gin.Context) {
-	user_id := c.Query("id")
-	group_name := c.Query("groupName")
-	description := c.Query("description")
+	user_id := c.Request.FormValue("id")
+	group_name := c.Request.FormValue("groupName")
+	description := c.Request.FormValue("description")
+	file, _, err := c.Request.FormFile("image")
+	var imageBytes []byte
+	if err == nil {
+		imageBytes, err = io.ReadAll(file)
+		fmt.Println(imageBytes, "imageBytes")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read image stream"})
+			return
+		}
+		file.Close()
+	} else {
+		// If there's an error, it means no image was sent or the field name "image" didn't match
+		fmt.Println("No image uploaded or error encountered:", err)
+	}
 	u := models.Groupslist{}
 	u.CreatedByUserId = user_id
 	u.GroupName = group_name
 	u.Description = description
-	_, err := u.SaveGroup()
+	u.ImageData = imageBytes
+
+	_, err = u.SaveGroup()
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": "Group Success"})
+
+	c.JSON(http.StatusOK, gin.H{"message": "Group created with byte data"})
 
 }
 
@@ -47,6 +66,12 @@ func GetGroupByuserId(c *gin.Context) {
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
+	}
+	for i := range groupsList {
+		if len(groupsList[i].ImageData) > 0 {
+			groupsList[i].ImageDataBase64 =
+				base64.StdEncoding.EncodeToString(groupsList[i].ImageData)
+		}
 	}
 	c.JSON(http.StatusOK, gin.H{"groups": groupsList})
 
